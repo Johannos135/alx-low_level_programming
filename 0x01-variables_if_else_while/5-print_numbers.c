3cbdc4e6d9226002d34a3d16aaa7046929f9d769
Johanne Essiere <essierejohanne1@gmail.com>
2023-02-17 16:22:54 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print digits from 0 to 10
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; i++)
+		printf(n);
+	putchar('\n');
+
+	return (0);
+}
