256d963bdb2f4c649f70d382506720e66a1f570f
Johanne Essiere <essierejohanne1@gmail.com>
2023-02-17 16:50:51 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print base16 numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+		putchar(num);
+
+	for (num = 97; num < 103; num++)
+		putchar(num);
+
+	putchar('\n');
+
+	return (0);
+}
