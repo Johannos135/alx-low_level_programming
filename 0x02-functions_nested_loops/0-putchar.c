dde022a7d7dab1fde23bead2a13d010f761b58db
Johanne Essiere <essierejohanne1@gmail.com>
2023-02-21 01:54:58 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Print _putchar followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int _size, count;
+
+	_size = sizeof(str) / sizeof(int);
+
+	for (count = 0; count < _size; count++)
+	{
+		_putchar(str[count]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+# ALX PROJECT 0x02. C - Functions, nested loops
+## _Project done during my ALX Software Engineering Program_
+
+
+- 0._putchar
