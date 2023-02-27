d32f05a7386590827f6cec22d6ca4667ad54b154
Johanne Essiere <essierejohanne1@gmail.com>
2023-02-27 15:16:32 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse followed
+ * by a new line
+ * @s: input string
+ */
+void print_rev(char *s)
+{
+	int counter;
+
+	counter = 0;
+
+	while (counter >= 0)
+	{
+		if (s[counter] == '\0')
+			break;
+		counter++;
+	}
+
+	for (counter--; counter >= 0; counter--)
+	{
+		_putchar(s[counter]);
+	}
+	_putchar('\n');
+}
