3b8969c6360c5e6eaf20506276f9d618b926cb42
Johanne ESSIERE <essierejohanne1@gmail.com>
2023-10-06 19:23:07 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,7 +2,7 @@
 
 /**
  * hash_table_set - Add or update an element in a hash table.
- * @ht: A pointer to the hash table.
+ * @hash: A pointer to the hash table.
  * @key: The key to add - cannot be an empty string.
  * @value: The value associated with key.
  *
@@ -10,26 +10,26 @@
  *         Otherwise - 1.
  */
 
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+int hash_table_set(hash_table_t *hash, const char *key, const char *value)
 {
 	hash_node_t *new;
 	char *value_copy;
 	unsigned long int index, i;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (hash == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+	index = key_index((const unsigned char *)key, hash->size);
+	for (i = index; hash->array[i]; i++)
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		if (strcmp(hash->array[i]->key, key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			free(hash->array[i]->value);
+			hash->array[i]->value = value_copy;
 			return (1);
 		}
 	}
@@ -47,8 +47,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	new->next = hash->array[index];
+	hash->array[index] = new;
 
 	return (1);
 }
